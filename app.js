fb9aafb009afd8013582eb3fd3995ec909f201ef
Vovin <p.rogozinski@gmail.com>
2011-07-28 21:44:22 +0000

    db

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var express = require('express');
 
 var app = module.exports = express.createServer();
 
+var alfred = require('alfred');
+var db;
+
 // Configuration
 app.configure(function() {
     app.set('views', __dirname + '/views');
@@ -35,10 +38,56 @@ app.configure('production', function() {
 
 // Routes
 app.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Express'
+    var blogs = [];
+
+    db.blogs.scan(function(err, key, value) {
+        if (err) {
+            throw err;
+        }
+        if (err === null && key === null) {
+            return res.render('index', {
+                title: 'Express',
+                blogs: blogs
+            });
+        }
+        blogs.push(value);
+    }, true);
+});
+
+app.get('/add/:address', function(req, res) {
+    addNewBlog(req.params.address, function(err, result){
+        if(err) { throw err; }
+        res.render('addResult', {
+            title: "adding new blog item",
+            result: result, 
+            address: req.params.address
+        });
     });
 });
 
 app.listen(process.env.C9_PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+
+
+alfred.open('db', function(err, database) {
+  if (err) { throw err; }
+  db = database;
+  db.ensure('blogs', function(err, users_key_map) {
+    console.log('blogs key map attached');
+  });
+});
+
+
+function addNewBlog(adr, next){
+    db.blogs.put(adr, {url: adr, name: adr},function(validationErrors){
+        if(validationErrors){
+            next(null, "failed" + validationErrors);
+        }else{
+            next(null, "success");
+        }
+    });
+}
+
+
+
